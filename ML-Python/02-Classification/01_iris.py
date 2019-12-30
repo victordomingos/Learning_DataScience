709ad89eceef6e9bdc35c08d171f78d64cb5123d
victordomingos <editor.arcosonline@gmail.com>
2019-12-30 02:26:53 +0000

    02-Classification/01_iris.py

---
+++ ML-Python/02-Classification/01_iris.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn import datasets
+
+pd.options.display.float_format = '{:20,.4f}'.format
+
+iris = datasets.load_iris()
+
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+df['target'] = iris.target
+df['species'] = [iris.target_names[i] for i in iris.target]
+
+print(pd.concat([df.head(3), df.tail(3)]))
+
+sns.pairplot(data=df, vars=df.columns[0:4], hue='species', height=1.5)
+plt.show()
+print(df['target'].describe())
